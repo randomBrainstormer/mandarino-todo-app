f6e74c8918073ebdc4be1c5de7b931ab880f36c2
Ivan Severino <ivan.r.severino@gmail.com>
2018-04-08 21:02:13 +0200

    placeholder per la pagina dei ToDos

---
+++ src/components/TodoListPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import './LoginPage.css';
+// import { TextField, Paper, Button } from 'material-ui';
+
+class TodoListPage extends Component {
+
+  handleLogin = () => {
+    console.log('Loging in....');
+  }
+  
+  render() {
+    return (
+      <div className="TodoListPage">
+        THIS IS A TODO
+      </div>
+    );
+  }
+}
+
+export default TodoListPage;
