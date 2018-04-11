01cd52f1d58a8c4052c680362f38f14b8c8157f2
Ivan Severino <ivan.r.severino@gmail.com>
2018-04-11 02:51:27 +0200

    placeholder per il Todo List

--- src/components/TodoListPage.js
+++ src/components/TodoListPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 // import './LoginPage.css';
-// import { TextField, Paper, Button } from 'material-ui';
+import { List, ListItem, ListItemText, ListItemSecondaryAction, Checkbox, IconButton, Icon,
+Grid, TextField, AppBar, Toolbar} from 'material-ui';
+import { Link } from 'react-router-dom'; 
 
 class TodoListPage extends Component {
 
@@ -11,7 +13,42 @@ class TodoListPage extends Component {
   render() {
     return (
       <div className="TodoListPage">
-        THIS IS A TODO
+        <AppBar position="static" color="default">
+          <Toolbar className="Dashboard-toolbar">
+          <Link to="/"><Icon>navigate_before</Icon></Link>
+            <h2>TO-DO App</h2>
+          </Toolbar>
+        </AppBar>
+        <Grid container spacing={8} alignItems="flex-end">
+          <Grid item>
+            <Icon>add</Icon>
+          </Grid>
+          <Grid item>
+            <TextField id="input-with-icon-grid" label="Aggiungi un ToDo" />
+          </Grid>
+        </Grid>
+        <List>
+          <ListItem
+            // key={i}
+            role={undefined}
+            dense
+            button
+            // onClick={this.handleToggle(value)}
+            // className={classes.listItem}
+          >
+            <Checkbox
+              // checked={this.state.checked.indexOf(value) !== -1}
+              tabIndex={-1}
+              disableRipple
+            />
+            <ListItemText primary={`Line item ${1 + 1}`} />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Comments">
+              <Icon>comment</Icon>
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </List>
       </div>
     );
   }
